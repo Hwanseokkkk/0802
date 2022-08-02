16c4446e9c2e57ffd5b2983e7b1612718b4aff1e
Hwanseokkkk <5038256@gmail.com>
2022-08-02 20:08:59 +0900

    hw

---
+++ src/Hw.java
@@ -0,0 +1,15 @@
+
+//public class Hw {
+	//public static void main(String[] args) {
+//		String str = "10";
+//		byte value = Byte.parseByte(str);
+//		System.out.println(str+1);
+		//String str = "" + 1;
+		//int su = Integer.parseInt(str);
+//		int x =  5;
+//		int y = 2;
+//		double result = (double)x/y;
+		
+	///	System.out.println();
+//	}
+//}
